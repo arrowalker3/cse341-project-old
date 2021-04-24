796cc3af14983fb47ed44dfa2671af86320590b8
arrowalker3 <arrowalker3@gmail.com>
2021-04-23 21:44:32 -0600

    Added ability to save user names and list new ones entered

--- routes/prove01-routes.js
+++ routes/prove01-routes.js
@@ -1,19 +1,30 @@
 const fs = require('fs');
+const { reset } = require('nodemon');
 
 
 const requestHandler = (req, res) => {
     const url = req.url;
     const method = req.method;
 
+    // In testing, just to make sure it works
+    // console.log('Requested page ', url);
+
     if (url === '/') {
         res.write('<html>');
+        // Title of HTML
         res.write('<head><title>Greetings</title></head>');
-        // Name of action below does not need to be message, we are setting a variable name to use later
+
+        // Body of HTML
         res.write('<body>');
         res.write('<h1>Good morning (or evening)!</h1>');
+
+        // Form on page
         res.write('<form action="/create-user" method="POST">');
-        res.write('<input type="text" name="message"><button type="submit">Send</button>');
+        res.write('<input type="text" name="username">');       // Text input
+        res.write('<button type="submit">Send</button>');   // Submit button
         res.write('</form>')
+        res.write('<a href="/users">View all the users here!</a>')
+
         res.write('</body>');
         res.write('</html>');
         return res.end();
@@ -21,12 +32,51 @@ const requestHandler = (req, res) => {
 
     if (url ==='/users') {
         res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        // Name of action below does not need to be message, we are setting a variable name to use later
-        res.write('<body><ul><li>User 1</li><li>User 2</li><li>User 3</li></body>');
+        res.write('<head>');
+        res.write('<title>Enter Message</title>');
+        res.write('</head>');
+
+        res.write('<body>');
+        res.write('<h2>Our users are:</h2>')
+        // List of users
+        res.write('<ul>');
+        // Read file
+        let users = fs.readFileSync('./public/other/users_prove01.txt').toString().split('\n');
+
+        // Write file to html
+        for (var user of users) {
+            // console.log(users);
+            res.write('<li>' + user + '</li>');
+            // console.log(user);
+        }
+        res.write('</ul>');
+
+        res.write('<h3>That\'s all folks!</h3>');
+
+        res.write('<a href="/">Back to the main page</a>')
+
+        res.write('</body>');
         res.write('</html>');
         return res.end();
     };
+
+    if (url === '/create-user' && method === 'POST') {
+        
+        const body = [];
+        req.on('data', chunk => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const username = parsedBody.split('=')[1];
+            fs.appendFile('./public/other/users_prove01.txt', '\n' + username, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/users');
+                return res.end();
+            });
+        });
+    }
 };
 
 // Export: using just the filename "routes" will use the proper function in this case
