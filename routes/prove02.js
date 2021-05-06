1cd8043fed8da8e46ea4fbfb17aa94ec7be1beff
arrowalker3 <arrowalker3@gmail.com>
2021-05-05 19:30:02 -0600

    Finished Prove 02

--- routes/prove02.js
+++ routes/prove02.js
@@ -10,7 +10,17 @@ const router = express.Router();
 const products = []
 
 // If looking for icon, ignore request
-router.post('/pages/prove02display', (req, res, next) => {
+router.post('/prove02display', (req, res, next) => {
+    products.push({book: req.body.book, summary: req.body.summary});
+
+    res.render('pages/prove02display', {
+        prods: products,
+        docTitle: 'Prove 02',
+        path: '/pages/prove02display',
+        hasProducts: (products.length > 0),
+        activeShop: true,
+        productCSS: true
+    });
 });
 
 router.get('/', (req, res, next) => {
